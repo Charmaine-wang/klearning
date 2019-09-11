5fea8300fdf583f1a19b47832e28ceb376ff5307
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-09-11 14:50:06 +0200

    added navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledNavbar = styled.div `
+    display:flex;
+`
+
+const Navbar = () => {
+    return (
+    <div>
+        <img src="" alt="" />
+    </div>
+    )
+}
+
+export default Navbar;
