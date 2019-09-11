72967859fce0ae9777b51a153c85c87105d024c7
charmaine wang <charmainewang424@hotmail.com>
2019-09-11 14:02:46 +0200

    cards

--- src/components/CourseList/index.js
+++ src/components/CourseList/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CourseListItem from '../CourseListItem'
 import styled from 'styled-components'
 
+
 const StyledCourseList = styled.div`
   width: ${props => (props.courseWidth ? "100%" : props.courseWidth)};
   height: 100%;
