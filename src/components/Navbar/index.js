77c67bb2d2af2021ceeb7342f1432078a01c9d9e
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-09-12 17:37:10 +0200

    Added some more code (unfisnished) to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,13 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledNavbar = styled.div `
-    display:flex;
+    // display:flex;
+    // width: 100%;
+    // height: 200px;
+    // position: fixed;
+
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      .nav-links {
+        display: flex;
+      }
 `
 
 const Navbar = () => {
     return (
-    <div>
-        <img src="" alt="" />
+    <div className="navbar">
+        <div className="nav-links">
+        <img src="logo1.png" alt="" />
+        </div>
+        <img src="logo1.png" alt="" />
+        <img src="logo1.png" alt="" />
     </div>
     )
 }
