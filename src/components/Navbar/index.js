25bb344d588f1da3166d2ce2f3c3d4140620f013
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-09-12 20:40:19 +0200

    add routing and pages-structure

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,45 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import GlobalStyle from "../../styles/GlobalStyle";
 
-const StyledNavbar = styled.div `
-    // display:flex;
-    // width: 100%;
-    // height: 200px;
-    // position: fixed;
+const StyledNavbar = styled.nav`
+  // display:flex;
+  // width: 100%;
+  // height: 200px;
+  // position: fixed;
 
-    .navbar {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-      }
-      .nav-links {
-        display: flex;
-      }
-`
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .nav-links {
+    display: flex;
+  }
+`;
 
 const Navbar = () => {
-    return (
-    <div className="navbar">
-        <div className="nav-links">
-        <img src="logo1.png" alt="" />
+  return (
+    <>
+      {/* Om globalstyle ligger i navbaren kommer den finnas med på hela sidan */}
+      <GlobalStyle />
+      <StyledNavbar>
+        <div className="navbar">
+          <div className="nav-links">
+            <img src="logo1.png" alt="" />
+          </div>
+          <img src="logo1.png" alt="" />
+          <img src="logo1.png" alt="" />
+          {/* NYTT, sorry om jag stökade till det för dig, men annars kan jag
+          inte titta på sidan jag håller på med. /Linn */}
+          <NavLink to="/home">Hem</NavLink>
+          <NavLink to="/course">Kurs</NavLink>
+          <NavLink to="/profile">Profil</NavLink>
         </div>
-        <img src="logo1.png" alt="" />
-        <img src="logo1.png" alt="" />
-    </div>
-    )
-}
+      </StyledNavbar>
+    </>
+  );
+};
 
 export default Navbar;
