b333de01d9fd8b52bb8afb82a9efb659f7666c86
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-09-13 10:31:41 +0200

    added temporary images and styled navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,39 +4,34 @@ import { NavLink } from "react-router-dom";
 import GlobalStyle from "../../styles/GlobalStyle";
 
 const StyledNavbar = styled.nav`
-  // display:flex;
-  // width: 100%;
-  // height: 200px;
-  // position: fixed;
-
-  height: 50%;
+  background-color: white;
+  
   .navbar {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding: 12px 24px;
   }
-  .nav-links {
+  
+  .navbarItems{
     display: flex;
+    justify-content: space-around;
+    width: 30%;
   }
 `;
 
 const Navbar = () => {
   return (
     <>
-      {/* Om globalstyle ligger i navbaren kommer den finnas med på hela sidan */}
       <GlobalStyle />
       <StyledNavbar>
         <div className="navbar">
-          <div className="nav-links">
-            <img src="logo1.png" alt="" />
+          <NavLink to="/home"><img src="images/logo.svg" alt="" /></NavLink>
+          {/* <NavLink to="/course">Kurs</NavLink> */}
+          <div className="navbarItems">
+            <img src="images/search.png" alt="" />
+            <NavLink to="/profile"><img src="images/profile.svg" alt="" /></NavLink>
           </div>
-          <img src="logo1.png" alt="" />
-          <img src="logo1.png" alt="" />
-          {/* NYTT, sorry om jag stökade till det för dig, men annars kan jag
-          inte titta på sidan jag håller på med. /Linn */}
-          <NavLink to="/home">Hem</NavLink>
-          <NavLink to="/course">Kurs</NavLink>
-          <NavLink to="/profile">Profil</NavLink>
         </div>
       </StyledNavbar>
     </>
