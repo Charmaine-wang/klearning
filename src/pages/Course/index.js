dcf49f853e7237d5e99c009ed472fae18a71aef6
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-09-13 11:43:25 +0200

    added styling to button

--- src/pages/Course/index.js
+++ src/pages/Course/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "../../components/Button";
 
 const StyledCourse = styled.div`
-margin-top:64px`;
+margin-top:64px
+
+.tempDiv{
+  display:flex;
+  width: 100%;
+  margin 24 px;
+  background-color: white;
+}
+
+`;
 
 const Course = props => {
   return (
     <StyledCourse>
       <h1>Här är kurssidan!</h1>
+      <div className="tempDiv">yo
+      <Button>FilmFilmFilm</Button>
+      <Button>TV</Button>
+      <Button>Film</Button>
+      <Button>Film</Button>
+      </div>
     </StyledCourse>
   );
 };
