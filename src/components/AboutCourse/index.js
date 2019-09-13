b740fdea73d87cb3beab7ffc5a9e99b8fb0c693b
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-09-13 15:11:08 +0200

    added aboutCourse component

---
+++ src/components/AboutCourse/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+import CourseInformation from "../../components/CourseInformation";
+
+
+const StyledAboutCourse = styled.div`
+
+
+`;
+
+const AboutCourse = (props) =>{
+
+    return(hej)
+ }
+
+ export default AboutCourse;
