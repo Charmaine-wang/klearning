951bd5432b019296e44d9e39c8f8db6a289e44b5
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-09-16 21:20:10 +0200

    fixed import

--- src/pages/Course/index.js
+++ src/pages/Course/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Button from "../../components/Button";
 import AboutCourse from "../../components/AboutCourse";
+import CourseEpisodes from "../../components/CourseEpisodes";
 
 const StyledCourse = styled.div`
 margin-top:64px;
