4c3ccb39982ed222295268ca8ae91ea5d4dce671
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-09-17 11:26:40 +0200

    add margins and more css to ActiveCourseCard

--- src/components/ActiveCourseCard/index.js
+++ src/components/ActiveCourseCard/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const StyledActiveCourseCard = styled.div`
   display: flex;
@@ -10,45 +10,47 @@ const StyledActiveCourseCard = styled.div`
   border-radius: 10px;
   justify-content: space-between;
   position: relative;
-  
+
   & > div {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: 5vh;  
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 5vh;
   }
-    & > div:first-of-type{
-        color: #C97C36;
-
+  & > div:first-of-type {
+    color: var(--soft-orange);
   }
 
-  
+  h5,
+  h2,
+  h3,
+  div > img {
+    padding: 0 15px;
+  }
+
+  h2 {
+    font-weight: 600;
+  }
 `;
-const ActiveCourseCard = (props) => {
+const ActiveCourseCard = props => {
+  return (
+    <StyledActiveCourseCard>
+      <img src={props.activeCourseImg} alt={props.activeCourseAlt} />
 
+      <div>
+        <h5>{props.courseCat}</h5>
+        <h5>{props.courseTime}</h5>
+      </div>
 
-    return (
- 
-        <StyledActiveCourseCard>
-            <img src={props.activeCourseImg} alt={props.activeCourseAlt}/>
-         
-            <div>
-                <h5>{props.courseCat}</h5>
-                <h5>{props.courseTime}</h5>
-            </div>
+      <h2>{props.cardHeader}</h2>
+      <h5>{props.cardParagraph}</h5>
 
-            <h2>{props.cardHeader}</h2>
-            <h5>{props.cardParagraph}</h5>
+      <div>
+        <h5>{props.courseTeacher}</h5>
+        <img src={props.bookmark} alt={props.bookmarkAlt} />
+      </div>
+    </StyledActiveCourseCard>
+  );
+};
 
-            <div>
-                <h5>{props.courseTeacher}</h5>
-                <img src={props.bookmark} alt={props.bookmarkAlt} />
-
-            </div>
-
-        </StyledActiveCourseCard>
-
-    )
-}
-
-export default ActiveCourseCard
+export default ActiveCourseCard;
