12765e0f6186d056e3885e9fe81c627db832f000
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-09-19 22:06:07 +0200

    finished HandInIntroCard

--- src/components/handin/HandIn/index.js
+++ src/components/handin/HandIn/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-import HandInCard from "../HandInCard";
+import HandInIntroCard from "../HandInIntroCard";
 
 const StyledHandIn = styled.div``;
 
 const HandIn = () => {
   return (
     <StyledHandIn>
-      <HandInCard
+      <HandInIntroCard
         introImage="images/selfTape.png"
         introHeaderParagraph="Skapa ett self-tape"
+        introParagraph1="Nu är det dags att spela in ditt eget self-tape!"
+        introParagraph2="Du laddar sedan upp din inspelning och får personlig feedback från din lärare."
+        introParagraph3="Du måste själv boka tid för feedback, instruktioner för detta följer."
       />
+      <h1>NÄSTA KORT ÄR NÄSTA SIDA</h1>
     </StyledHandIn>
   );
 };
--- src/components/handin/HandInIntroCard/index.js
+++ src/components/handin/HandInIntroCard/index.js
@@ -10,8 +10,23 @@ const StyledHandInCard = styled.div`
   justify-content: center;
   align-items: center;
 
-  .introduction-hand-in > p {
+  .intro-hand-in > p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    // font-weight: bold;
+    padding: 16px;
+  }
+
+  .intro-header-paragraph {
     font-weight: bold;
+  }
+
+  .intro-hand-in > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
@@ -24,9 +39,11 @@ const HandInCard = ({
 }) => {
   return (
     <StyledHandInCard>
-      <div className="introduction-hand-in">
-        <img src={introImage} alt="" />
-        <p>{introHeaderParagraph}</p>
+      <div className="intro-hand-in">
+        <div>
+          <img src={introImage} alt="" />
+        </div>
+        <p className="intro-header-paragraph">{introHeaderParagraph}</p>
         <p>{introParagraph1}</p>
         <p>{introParagraph2}</p>
         <p>{introParagraph3}</p>
