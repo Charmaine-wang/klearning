7124123d2fe3e0182385bda9c162eb8acdc7dd9a
charmaine wang <charmainewang424@hotmail.com>
2019-09-19 21:04:59 +0200

    modal and communication card

---
+++ src/components/Communication/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import People from "../Icons/People";
+import Button from "../Button";
+
+const StyledCommunication = styled.div`
+  display: flex;
+  width: 95vw;
+  height: 50vh;
+  padding: 5%;
+  /* position: absolute; */
+  flex-direction: column;
+  border-radius: 10px;
+  background: #ffffff;
+  justify-content: space-around;
+  align-items: center;
+  & > p {
+    line-height: 26px;
+    text-align: center;
+  }
+`;
+const Communication = props => {
+  return (
+    <StyledCommunication {...props}>
+      <h2>Kommunikation</h2>
+      <People />
+      <h3>Skype gruppen</h3>
+      <p>
+        Vi har skapat en skype grupp för dig och dina kursvänner, så att ni kan
+        hålla kontakt under kursens gång. Allt du behöver är ett skypekonto och
+        sedan är det bara att skriva på!
+      </p>
+      <Button btnName="Skype grupp" buttonWidth="157px" />
+    </StyledCommunication>
+  );
+};
+
+export default Communication;
