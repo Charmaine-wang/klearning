94dbc298eaaa092a0649270f65f948b347141b18
Linn <linn.johansson7@hotmail.com>
2019-09-19 16:07:42 +0200

    add temporary routes/pages

---
+++ src/pages/TempMediaPage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledMediaPage = styled.div`
+  margin-top: 74px;
+`;
+
+const MediaPage = () => {
+  return (
+    <StyledMediaPage>
+      <h1>This is the page where we test media components/views.</h1>
+    </StyledMediaPage>
+  );
+};
+
+export default MediaPage;
---
+++ src/pages/TempQuizPage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledQuizPage = styled.div`
+  margin-top: 74px;
+`;
+
+const QuizPage = () => {
+  return (
+    <StyledQuizPage>
+      <h1>This is the quizpage</h1>
+    </StyledQuizPage>
+  );
+};
+
+export default QuizPage;
